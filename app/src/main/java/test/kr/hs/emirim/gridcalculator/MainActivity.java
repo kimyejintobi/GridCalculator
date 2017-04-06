eef43cca6202ab3a556e0ca0c2b7a84d5d5b31c5
kimyejintobi <kimyejintobi@gmail.com>
2017-04-06 20:56:40 +0900

    #2 GridLayout

--- app/src/main/java/test/kr/hs/emirim/gridcalculator/MainActivity.java
+++ app/src/main/java/test/kr/hs/emirim/gridcalculator/MainActivity.java
@@ -2,12 +2,43 @@ package test.kr.hs.emirim.gridcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button[] butNums=new Button[10];
+    Button[] butOps=new Button[4];
+    //int[] ids={R.id.but_01, R.id.but_02, R.id.but_03, R.id.but_04, R.id.but_05, R.id.but_06, R.id.but_07, R.id.but_08, R.id.but_09, R.id.but_10}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        for(int i = 0; i<butNums.length; i++){
+            //butNums[i]=(Button)findViewById(ids+i);
+            butNums[i]=(Button)findViewById(R.id.but_01+i);
+            butNums[i].setOnClickListener(butNumHandler);
+        }
+        for(int i=0;i<butOps.length;i++){
+            butOps[i]=(Button)findViewById(R.id.button6+i);
+            butOps[i].setOnClickListener(butOpsHandler);
+        }
     }
+
+    View.OnClickListener butNumHandler=new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+
+        }
+
+    };
+    View.OnClickListener butOpsHandler=new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+
+
+        }
+    };
 }
